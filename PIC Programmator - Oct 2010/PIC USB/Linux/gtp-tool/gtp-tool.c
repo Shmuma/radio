d7805aa25aec93e5fef964641d12ac84ad21cf4e
Max Lapan <max.lapan@gmail.com>
2010-12-17 11:37:14 +0300

    [gtp] Find device

--- PIC Programmator - Oct 2010/PIC USB/Linux/gtp-tool/gtp-tool.c
+++ PIC Programmator - Oct 2010/PIC USB/Linux/gtp-tool/gtp-tool.c
@@ -5,18 +5,30 @@
 
 struct libusb_context* ctx;
 
+#define GTP_VENDOR  0x04D8
+#define GTP_PRODUCT 0x5200
 
-void find_gtp_device ()
+
+struct libusb_device* find_gtp_device ()
 {
-    ssize_t count;
+    ssize_t count, i;
+    libusb_device** dev;
+    struct libusb_device_descriptor descr;
 
-    count = libusb_get_device_list ()
+    count = libusb_get_device_list (ctx, &dev);
+
+    for (i = 0; i < count; i++)
+        if (!libusb_get_device_descriptor (dev[i], &descr))
+            if (descr.idVendor == GTP_VENDOR && descr.idProduct == GTP_PRODUCT)
+                return dev[i];
+    return NULL;
 }
 
 
 int main (int argc, char *argv[])
 {
     int err;
+    struct libusb_device* dev;
 
     if ((err = libusb_init (&ctx)) != 0) {
         printf ("libusb_init failed with code %d\n", err);
@@ -25,7 +37,12 @@ int main (int argc, char *argv[])
 
     libusb_set_debug (ctx, 3);
 
-    find_gtp_device ();
+    dev = find_gtp_device ();
+    if (!dev)
+        printf ("GTP-USB device not found\n");
+    else {
+        printf ("GTP-USB device found\n");
+    }
 
     libusb_exit (ctx);
     return 0;
