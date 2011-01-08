f54a05fac3f608b4e950bce79c81c490fda38275
Max Lapan <max.lapan@gmail.com>
2011-01-08 21:33:15 +0300

    Clock control added

--- study/pickit2/Timer0/timer.asm
+++ study/pickit2/Timer0/timer.asm
@@ -1,24 +1,33 @@
 #include <p16f887.inc>
 
-        __CONFIG        _CONFIG1, _DEBUG_ON & _LVP_OFF & _FCMEN_OFF & _IESO_OFF & _BOR_OFF & _CPD_OFF & _CP_OFF & _MCLRE_ON & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT
+        __CONFIG        _CONFIG1, _DEBUG_OFF & _LVP_OFF & _FCMEN_OFF & _IESO_OFF & _BOR_OFF & _CPD_OFF & _CP_OFF & _MCLRE_ON & _PWRTE_ON & _WDT_OFF & _INTRC_OSC_NOCLKOUT
         __CONFIG        _CONFIG2, _WRT_OFF & _BOR21V
 
 
         org     0
 
-        banksel TRISD
-        clrf    TRISD
-        banksel PORTD
-        clrf    PORTD
-        movlw   b'11111111'
-        movwf   PORTD
+        ;; bank 1
+        bsf     STATUS, RP0
+        bcf     STATUS, RP1
 
-        banksel TMR0
-        clrf    TMR0
-        banksel OPTION_REG
+        ;; output on D port
+        clrf    TRISD
+
+        ;; timer 0 prescaler 1:256
         movlw   B'00000111'
         movwf   OPTION_REG
-        
+
+        ;; switch to 31 KHz
+        bcf     OSCCON, IRCF0
+        bcf     OSCCON, IRCF1
+        bsf     OSCCON, IRCF2
+
+        ;; bank 0
+        bcf     STATUS, RP0
+
+        clrf    PORTD
+        decf    PORTD
+
 loop:
         btfss   INTCON, T0IF
         goto    loop
