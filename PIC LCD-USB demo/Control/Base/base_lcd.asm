925cc14a8597e80c22d9fd74fae2e2230c413027
Max Lapan <max.lapan@gmail.com>
2011-01-15 17:58:47 +0300

    Cannnot clear LCD

--- PIC LCD-USB demo/Control/Base/base_lcd.asm
+++ PIC LCD-USB demo/Control/Base/base_lcd.asm
@@ -11,39 +11,118 @@ cblock  20
         Delay1
         Delay2
 endc
+
+;;; LCD demo board constants
+RS      equ     RB0
+RW      equ     RB1
+E       equ     RB2
+DB0     equ     RA0
+DB1     equ     RA1
+DB2     equ     RA2
+DB3     equ     RA3
+DB4     equ     RA4
+DB5     equ     RA5
+DB6     equ     RB3             ; Note that B reg used
+DB7     equ     RB4
         
         org     0
 
         ;; output on A and B ports
         clrf    TRISA
+        clrf    TRISB
 
-Loop:   
-        ;; calibrate delay loop
-        bsf     PORTA, RA0
-        movlw   10
-        call    Delay2us
-        bcf     PORTA, RA0
-        movlw   20
-        call    Delay2us
-        goto    Loop
+        clrf    PORTA
+        clrf    PORTB
+        
+        ;; R/W -> 0, RS -> 0
+;;;     bcf     PORTB, RS
+;;;     bcf     PORTB, RW
 
-        ;; Delay subroutine. W reg must hold amount of 250 us delays
+;;;     bcf     PORTB, E
+
+        ;; wait for init completed
+        movlw   1
+        call    Delay250us
+
+        call    InitLCD
+        
+ClearLoop:
+        call    ClearLCD
+        goto    ClearLoop
+
+;;; Delay subroutine. W reg must hold amount of 250 us delays
 Delay250us:
         movwf   Delay2
-DelayLoop:
+Delay250Loop:
         decfsz  Delay1, f
-        goto    DelayLoop
+        goto    Delay250Loop
         decfsz  Delay2, f
-        goto    DelayLoop
+        goto    Delay250Loop
         return
 
-        ;; W reg must hold amount of 2 us delays
+;;; W reg must hold amount of 2 us delays
 Delay2us:
         movwf   Delay1
-Delay1Loop:     
+Delay2Loop:     
         decfsz  Delay1, f
-        goto    Delay1Loop
+        goto    Delay2Loop
+        return
+
+;;; Routine clears LCD
+ClearLCD:
+        movlw   1
+        call    Delay250us
+        
+        ;; RS = 0, RW = 0, DB1-DB7 = 0, DB0 = 1
+        bsf     PORTB, E
+
+        movlw   1
+        call    Delay250us
+        
+        bsf     PORTA, DB0
+
+        movlw   1
+        call    Delay250us
+
+        bcf     PORTB, E
+        
+        movlw   1
+        call    Delay250us
+
+        clrf    PORTA
+
+        movlw   1
+        call    Delay250us
+        return
+
+
+;;; Initializes LCD function
+InitLCD:
+        movf    PORTB, WREG
+        bsf     WREG, E
+        movwf   PORTB
+        movlw   1
+        call    Delay250us
+
+        bsf     PORTA, DB5      ; function set
+        bsf     PORTA, DB4      ; 8-bit mode
+        bsf     PORTA, DB3      ; 2 line display
+        bcf     PORTA, DB2      ; 8x5 dot matrix
+
+        movlw   1
+        call    Delay250us
+
+        bcf     PORTB, E
+
+        movlw   1
+        call    Delay250us
+
+        clrf    PORTA
+        clrf    PORTB
+
+        movlw   1
+        call    Delay250us
+        
         return
         end
-
         
