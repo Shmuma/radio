45087d7b18e0a5a108248b081eeea94fce61f347
Max Lapan <max.lapan@gmail.com>
2012-12-23 20:00:20 +0400

    Join of events

--- DCF77-clock/dcf77proto/decode.py
+++ DCF77-clock/dcf77proto/decode.py
@@ -3,44 +3,93 @@
 import sys
 
 
-bits = []
+class Event (object):
+    def __init__ (self, sec, usec, bit):
+        self.sec = sec
+        self.usec = usec
+        self.bit = bit
+        
+    def __str__ (self):
+        return "Event @%d,%d: %d" % (self.sec, self.usec, self.bit)
+
+
+    def abs_time (self, cycle, prev_sec):
+        """
+        Turn event into absolute time, detecting wrap
+        """
+        inc = self.sec < prev_sec
+        if inc:
+            cycle += 1
+        self.sec += cycle * 65
+        self.usec += cycle * 536
+        self.sec += int (self.usec / 1000)
+        self.usec %= 1000
+        return inc
+
+
+    def distance (self, ev):
+        """
+        Return time distance between two events in miliseconds
+        """
+        dsec = self.sec - ev.sec
+        dusec = self.usec - ev.usec
+        return dsec * 1000 + dusec       
+
+
+    @classmethod
+    def parse (c, l):
+        v = l.split (': ')
+        if len (v) != 2:
+            return None
+        t = v[0].split (', ')
+        if len (t) != 2:
+            return None
+        sec, usec = map (int, t)
+        dat = v[1].split (', ')
+        if len (dat) != 2:
+            return None
+        bit = int (dat[0])
+        return c (sec, usec, bit)
+
+
+# first - parse events
+events = []
+cycle = 0
 prev_sec = None
 
 for l in sys.stdin:
     l = l.strip ()
     if len (l) == 0:
         continue
-    v = l.split (': ')
-    if len (v) != 2:
-        continue
-    t, dat = v
-    # second
-    sec = int (t.split (',')[0])
-    bit = dat.split (',')[0]
-
-    if prev_sec == sec:
-        if bits[-1] != bit:
-            bits[-1] = '*'
-    else:
+    ev = Event.parse (l)
+    if ev != None:
         if prev_sec != None:
-            if sec > prev_sec:
-                count = sec - prev_sec - 1
-            else:
-                count = sec + 65 - prev_sec - 1
-            bits += ['_'] * count
-        bits.append (bit)
-        prev_sec = sec
+            sec = ev.sec
+            if ev.abs_time (cycle, prev_sec):
+                cycle += 1
+            prev_sec = sec
+        else:
+            prev_sec = ev.sec
+        events.append (ev)
 
-longest = 0
-cur = 0
 
-for b in bits:
-    if b in ['0', '1']:
-        cur += 1
-    else:
-        longest = max (longest, cur)
-        cur = 0
+def join_events (usecs, events):
+    """
+    Join events, nearer than given time together
+    """
+    start = events[0]
+    joined = [start]
+    
+    for e in events[1:]:
+        d = e.distance (start)
+        if d < usecs:
+            joined.append (e)
+        else:
+            yield start
+            start = e
+            joined = [e]
 
-print bits
-print len (bits)
-print longest
+
+j_events = [e for e in join_events (450, events)]
+
+print "%d events joined into %d" % (len (events), len (j_events))
